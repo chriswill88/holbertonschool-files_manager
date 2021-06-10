9f6b250da6979a6d5c6d053c57a30162d1b9aadd
chriswill88 <williechri79@gmail.com>
2021-06-10 14:49:49 -0400

    [0 - promisify in method]

--- utils/redis.js
+++ utils/redis.js
@@ -5,6 +5,10 @@ class RedisClient {
   constructor() {
     this.client = redis.createClient();
     this.client.on('error', (error) => console.error(error));
+
+    this.getter = promisify(this.client.get).bind(this.client);
+    this.setter = promisify(this.client.set).bind(this.client);
+    this.deleter = promisify(this.client.del).bind(this.client);
   }
 
   isAlive() {
@@ -12,18 +16,15 @@ class RedisClient {
   }
 
   async get(key) {
-    const getter = promisify(this.client.get).bind(this.client);
-    await getter(key);
+    await this.getter(key);
   }
 
   async set(key, value, duration) {
-    const setter = promisify(this.client.set).bind(this.client);
-    await setter(key, value, 'EX', duration);
+    await this.setter(key, value, 'EX', duration);
   }
 
   async del(key) {
-    const deleter = promisify(this.client.del).bind(this.client);
-    await deleter(key);
+    await this.deleter(key);
   }
 }
 
