43d27d9db61c90dc641ce16d8f874b23f32ff2ce
matxa <42968545+matxa@users.noreply.github.com>
2021-06-23 20:25:55 -0400

    created endpoints /connect, /disconnect, and /users/me

---
+++ controllers/AuthController.js
@@ -0,0 +1,72 @@
+import { ObjectID } from 'bson';
+import sha1 from 'sha1';
+import { uuid } from 'uuidv4';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+const DB_DATABASE = process.env.DB_DATABASE ? process.env.DB_DATABASE : 'files_manager';
+const users = dbClient.client.db(DB_DATABASE).collection('users');
+
+export default class AuthController {
+  static async getConnect(req, res) {
+    const base64 = req.headers.authorization;
+
+    if (base64) {
+      // get basic64 value
+      const [, userInfoBase64] = base64.split(' ');
+      // decode userInfoBase64
+      const buff = Buffer.from(userInfoBase64, 'base64');
+      // cast buff to string
+      const userInfoString = buff.toString('utf-8');
+      // get email and pwd from userInfoString
+      const [userEmail, userPwd] = userInfoString.split(':');
+
+      // find user in the database
+      const dbUser = await users.findOne({ email: userEmail });
+      // authenticate user
+      if (dbUser && dbUser.password === sha1(userPwd)) {
+        // create token to save in Redis { auth_<token>: dbUser.id }
+        const genToken = uuid();
+        redisClient.set(genToken, dbUser._id, 60 * 60 * 24);
+        // RESPONSE
+        res.status(200).send({ token: genToken });
+      } else {
+        res.status(401).send({ error: 'Unauthorized' });
+      }
+    }
+  }
+
+  static async getDisconnect(req, res) {
+    const xToken = req.headers['x-token'];
+
+    if (xToken) {
+      // get userId from redis
+      const currentUserId = await redisClient.get(xToken);
+      // get user from DB using userId
+      const userInDb = await users.findOne({ _id: ObjectID(currentUserId) });
+      if (userInDb) {
+        redisClient.del(xToken);
+        res.status(204).send();
+      } else {
+        res.status(401).send({ error: 'Unauthorized' });
+      }
+    }
+  }
+
+  static async getMe(req, res) {
+    const xToken = req.headers['x-token'];
+
+    if (xToken) {
+      // get userId from redis
+      const currentUserId = await redisClient.get(xToken);
+      // get user from DB using userId
+      const userInDb = await users.findOne({ _id: ObjectID(currentUserId) });
+      if (userInDb) {
+        delete userInDb._id;
+        res.status(200).send(userInDb);
+      } else {
+        res.status(401).send({ error: 'Unauthorized' });
+      }
+    }
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const statusRoute = express.Router();
 const statsRoute = express.Router();
@@ -10,6 +11,9 @@ const authRoute = express.Router();
 statusRoute.get('/status', AppController.getStatus);
 statsRoute.get('/stats', AppController.getStats);
 userRoute.post('/users', UsersController.postNew);
+authRoute.get('/connect', AuthController.getConnect);
+authRoute.get('/disconnect', AuthController.getDisconnect);
+authRoute.get('/users/me', AuthController.getMe);
 
 export {
   statusRoute, statsRoute, userRoute, authRoute,
