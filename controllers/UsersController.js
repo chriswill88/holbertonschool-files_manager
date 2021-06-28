cb2513f4e723f5c7bb625385b841a3e0481e06ab
matxa <42968545+matxa@users.noreply.github.com>
2021-06-27 22:09:14 -0400

    refactor code

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -2,7 +2,6 @@ import sha1 from 'sha1';
 import { ObjectID } from 'bson';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
-import { del } from 'request';
 
 const DB_DATABASE = process.env.DB_DATABASE ? process.env.DB_DATABASE : 'files_manager';
 const users = dbClient.client.db(DB_DATABASE).collection('users');
@@ -14,8 +13,8 @@ export default class UsersController {
     const { password } = req.body;
 
     // check if credentials are present
-    if (!email) res.status(400).send({ error: 'Missing email' });
-    if (!password) res.status(400).send({ error: 'Missing password' });
+    if (!email) return res.status(400).send({ error: 'Missing email' });
+    if (!password) return res.status(400).send({ error: 'Missing password' });
 
     // get user's email from the database
     const users = dbClient.client.db(DB_DATABASE).collection('users');
@@ -26,12 +25,8 @@ export default class UsersController {
     else await users.insertOne({ email, password: sha1(password) });
 
     // response
-    const response = await users.findOne({ email })
-    if (response) {
-      response.id = response._id
-      delete response._id
-    }
-    res.status(201).send(response);
+    const response = await users.findOne({ email });
+    return res.status(201).send(response);
   }
 
   static async getMe(req, res) {
@@ -44,10 +39,10 @@ export default class UsersController {
       const userInDb = await users.findOne({ _id: ObjectID(currentUserId) });
       if (userInDb) {
         delete userInDb._id;
-        res.status(200).send(userInDb);
-      } else {
-        res.status(401).send({ error: 'Unauthorized' });
+        return res.status(200).send(userInDb);
       }
+      return res.status(401).send({ error: 'Unauthorized' });
     }
+    return res.status(401).send({ error: 'Unauthorized' });
   }
 }
